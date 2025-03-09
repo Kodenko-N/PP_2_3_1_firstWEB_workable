9ec8944201ba8790c072d73109a084a6c2b6bcbc
Kodenko-N <kodenko.n@gmail.com>
2025-03-09 23:00:36 +0300

    v.0.2

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,62 @@
+package web.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String sureName;
+
+    public User() {
+    }
+
+    public User(String name, String sureName) {
+        this.name = name;
+        this.sureName = sureName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSureName() {
+        return sureName;
+    }
+
+    public void setSureName(String sureName) {
+        this.sureName = sureName;
+    }
+
+    @Override
+    public String toString() {
+        return "User name is " + name + ", FamilyName is " + sureName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(sureName, user.sureName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, sureName);
+    }
+}
